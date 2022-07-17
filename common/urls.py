f7c25114c9ad027147ce112d494b7947fb6a91af
orifjohn <orifjondev@gmail.com>
2022-07-17 15:49:18 +0500

    feat: templates

--- common/urls.py
+++ common/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from .views import index_view
+from .views import index_view, good_habits, bad_habits
 
 urlpatterns = [
     path("", index_view, name='index-url'),
+    path("good/", good_habits, name='good_habits_url'),
+    path("bad/", bad_habits, name='bad_habits_url')
 ]
--- common/views.py
+++ common/views.py
@@ -5,5 +5,15 @@ from .models import Habit
 # Create your views here.
 
 def index_view(request):
-    habit = Habit.objects.all()
-    return render(request, 'index.html', {'habit': habit})
+    habits = Habit.objects.all()
+    return render(request, 'main/index.html', {'habits': habits})
+
+
+def good_habits(request):
+    goods = Habit.objects.filter(is_good=False).all()
+    return render(request, 'main/good.html', {'goods': goods})
+
+
+def bad_habits(request):
+    bads = Habit.objects.filter(is_good=True).all()
+    return render(request, 'main/bad.html', {'bads': bads})
